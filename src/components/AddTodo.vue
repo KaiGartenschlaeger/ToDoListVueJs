61a4528994af6321ae6338c9daf791dce3ac15ca
Kai Gartenschläger <mail@kaisnet.de>
2019-02-24 00:13:00 +0100

    Grundgerüst hinzugefügt

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" name="title" v-model="title" placeholder="Add todo..">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  methods: {
+    addTodo(e) {
+        e.preventDefault();
+
+        // create todo
+        const newTodo = {
+            title: this.title
+        };
+
+        // send up to parent
+        this.$emit('add-todo', newTodo);
+
+        // reset form
+        this.title = '';
+    }
+  },
+  data() {
+      return {
+          title: ''
+      }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-completed':todo.completed}">
+    <p>
+      <input type="checkbox" 
+        v-bind:id="'item_' + todo.id" 
+        v-bind:checked="todo.completed"
+        v-on:change="markComplete">&nbsp;
+      <label v-bind:for="'item_' + todo.id">{{todo.title}}</label>
+
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+      markComplete() {
+          this.todo.completed = !this.todo.completed;
+      }
+  }
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: #ccc dotted;
+}
+
+.todo-item.is-completed label {
+  text-decoration: line-through;
+  color: #aaa;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
